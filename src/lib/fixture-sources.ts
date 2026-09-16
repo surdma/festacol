835e32e04a01a5878521bcbd56f7ffcce7cba4c7
Surdma <devsurdma@gmail.com>
2026-09-16 01:51:09 +0100

    refactor(fixtures): centralize question source manifest

---
+++ src/lib/fixture-sources.ts
@@ -0,0 +1,11 @@
+export const QUESTION_FIXTURE_SCHEMA_VERSION = 4;
+
+export const QUESTION_FIXTURE_FILES = [
+  "questions.json",
+  "questions/qualifier-english.json",
+  "questions/qualifier-mathematics.json",
+  "questions/qualifier-basic-science.json",
+  "questions/qualifier-humanities.json",
+  "questions/qualifier-business.json",
+  "questions/qualifier-digital.json",
+] as const;
